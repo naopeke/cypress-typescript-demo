0efbdcb8e65d2336a158a16d98012cc99621ab74
naopeke <naopeke@protonmail.com>
2025-03-06 10:47:48 +0100

    typescript

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,26 @@
 import * as lists from '../fixtures/lists.json';
+
 it('test', ()=>{
     cy.visit('/');
     cy.contains('Get started!').should('be.visible');
     cy.get('.mb-8').invoke('text');
-    cy.get('[data-cy="board-item"]').realHover();
+    cy.get('[data-cy="first-board"]').should('be.visible').type('new board{enter}');
+    // cy.get('[data-cy="board-item"]').realHover();
+})
 
-    cy.get('[data-cy="first-board"]').type('new board{enter}');
+it('testing with typescript', ()=>{
+    //POSTリクエストが/api/listsに送信されるのを監視。.as('listCreate')で、このリクエストをlistCreateという名前でエイリアス（別名）をつける。
+    cy.intercept('POST', '/api/lists').as('listCreate');
+
+    cy.visit('/board/1');
+
+    lists.forEach( list =>{
+
+        cy.get('[data-cy="add-list-input"]').type(`${list.name}{enter}`);
+    
+        //先ほどエイリアスをつけたlistCreateというAPIリクエストが完了するのを待機
+        cy.wait('@listCreate')
+            .its('response.body.order') //そのリクエストのレスポンス（response.body）からorderフィールドを取得。.its() は、Cypressのコマンドの一つで、取得したオブジェクトのプロパティにアクセスするために使用
+            .should('eq', list.order); //取得したorderが、lists配列に定義された順番（list.order）と一致することを確認
+    })
 })
